5d76610671cb96276674c64efddccf736ccdb490
hfhbd <hfhbd@users.noreply.github.com>
2020-12-04 12:09:54 +0100

    Init Commit

---
+++ build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    kotlin("multiplatform") version "1.4.20"
+    kotlin("plugin.serialization") version "1.4.20"
+}
+
+group = "org.hfhbd"
+version = "0.0.1"
+
+repositories {
+    jcenter()
+    maven(url = "https://dl.bintray.com/cy6ergn0m/uuid")
+    maven(url = "https://kotlin.bintray.com/kotlinx/") // kotlinx.datetime
+}
+
+kotlin {
+    explicitApi()
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+    }
+    js(BOTH) {
+        browser {
+            testTask {
+                useKarma {
+                    useChromeHeadless()
+                }
+            }
+        }
+        nodejs()
+    }
+
+    
+    sourceSets {
+        commonMain {
+            dependencies {
+                api("io.ktor:ktor-client-cio:1.4.3")
+                api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+                api("org.jetbrains.kotlinx:kotlinx-datetime:0.1.1")
+
+                api("org.jetbrains.kotlinx.experimental:kotlinx-uuid-core:0.0.2")
+            }
+        }
+        commonTest {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+            }
+        }
+        val jsMain by getting
+        val jsTest by getting {
+            dependencies {
+                implementation(kotlin("test-js"))
+            }
+        }
+    }
+}
