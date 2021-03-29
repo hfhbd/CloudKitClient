053c2a66add852b0f50c5b236b070ca2bcf9c570
hfhbd <hfhbd@users.noreply.github.com>
2021-03-29 16:47:35 +0200

    Switch from ktor base64 to own implementation

--- core/src/commonMain/kotlin/app/softwork/cloudkitclient/internal/base64.kt
+++ core/src/commonMain/kotlin/app/softwork/cloudkitclient/internal/base64.kt
@@ -4,6 +4,8 @@ internal expect val String.encodeBase64: String
 
 internal expect val String.decodeBase64: String
 
+internal expect val String.decodeBase64Bytes: ByteArray
+
 internal expect val ByteArray.decodeBase64: String
 
 internal expect val ByteArray.encodeBase64: String
