aa5edb6bb861d007e84e89d57b5ece51d5a46853
hfhbd <hfhbd@users.noreply.github.com>
2021-06-24 23:27:56 +0200

    Remove flanky integrationTests from CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,6 @@ rootProject.name = "cloudkitclient"
 include(":cloudkitclient-core")
 include(":cloudkitclient-testing")
 
-val keyID: String? = System.getProperty("keyID")
-if (keyID != null) {
+System.getProperty("keyID")?.let {
     include(":integrationTest")
 }
