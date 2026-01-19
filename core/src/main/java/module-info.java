1f4b8eb60ff8ec07ba6deb955a5435f09998a88d
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2026-01-19 20:12:29 +0100

    Remove develocity plugin (#345)

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -5,6 +5,5 @@ module app.softwork.cloudkitclient {
     exports app.softwork.cloudkitclient.values;
 
     requires kotlin.stdlib;
-    requires transitive io.ktor.client.java;
     requires transitive kotlinx.serialization.json;
 }
--- gradle/build-logic/settings.gradle.kts
+++ gradle/build-logic/settings.gradle.kts
@@ -2,7 +2,6 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         mavenCentral()
-        gradlePluginPortal()
     }
     versionCatalogs.register("libs") {
         from(files("../libs.versions.toml"))
